8a58d57b5642dd5a0951752ee1da6547f16c610a
陈金金杰 <739917425@qq.com>
2019-04-19 22:31:07 +0800

    add the Hash

--- Template/String/Hash.cpp
+++ Template/String/Hash.cpp
@@ -8,7 +8,7 @@ void init(char* s, int n)	// use 131 or 13331 to be the base
 		p[i]=p[i-1]*131;
 	}
 }
-unsigned long long hash(char* s,int l,int r)
+unsigned long long Hash(int l,int r)
 {
 	return f[r]-f[l-1]*p[r-l+1];
 }
