aab8472f29ed2cb5c813a3c8996ec2df53a25ddf
陈金金杰 <739917425@qq.com>
2019-06-16 23:01:24 +0800

    update Digit_DP

--- Template/Others/Digit_DP.cpp
+++ Template/Others/Digit_DP.cpp
@@ -1,39 +1,17 @@
 int dig[maxn];
-ll pw[maxn];
+ll pw[maxn]; // pw[i]=10^i
 P dp[maxn][5];
 bool vis[maxn][5]; 
 P dfs(int pos,int sta,int f){
-	if (pos<0){
-		return sta==3?mp(1,0):mp(0,0);
-	}
+	if (pos<0)
+		return check(sta)?mp(1,0):mp(0,0);
 	if (!f&&vis[pos][sta])
 		return dp[pos][sta];
 	int up=f?dig[pos]:9;
 	P ans,tmp;
 	ans.fi=0,ans.se=0;
 	for (int i=0;i<=up;++i){
-		if (sta==0){
-			if (i==8)
-				tmp=dfs(pos-1,1,f&&i==up);
-			else
-				tmp=dfs(pos-1,0,f&&i==up);
-		}
-		else if (sta==1){
-			if (i==8)
-				tmp=dfs(pos-1,2,f&&i==up);
-			else 
-				tmp=dfs(pos-1,0,f&&i==up);
-		}
-		else if (sta==2){
-			if (i==6)
-				tmp=dfs(pos-1,3,f&&i==up);
-			else if (i==8)
-				tmp=dfs(pos-1,2,f&&i==up);
-			else
-				tmp=dfs(pos-1,0,f&&i==up);
-		}
-		else
-			tmp=dfs(pos-1,3,f&&i==up);
+		tmp=dfs(pos-1,new_sta,f&&i==up); 
 		ans.fi=add(ans.fi,tmp.fi);
 		ans.se=add(ans.se,add(mul(mul(tmp.fi,i),pw[pos]),tmp.se));
 	}
