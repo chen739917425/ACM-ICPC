2daec75ff8539fec2296900e7f3d1ae37210dd6a
陈金金杰 <739917425@qq.com>
2019-09-19 03:19:42 +0800

    add poj 2778 in AC_auto

--- Template/String/AC_automaton.cpp
+++ Template/String/AC_automaton.cpp
@@ -72,3 +72,80 @@ struct Trie
 		}
 	}
 }trie;
+/*
+	poj 2778 DNA Sequence
+	AC 自动机跑出邻接矩阵
+	矩阵快速幂求方案数 
+*/
+int rt,id,hs[maxn],ne[maxn][10],vis[maxn],fail[maxn];
+inline int newnode(){
+	++id;
+	for (int i=0;i<4;++i)
+		ne[id][i]=0;
+	return id;
+}
+void init(){
+	hs['A']=0;
+	hs['C']=1;
+	hs['T']=2;
+	hs['G']=3;
+	id=-1;
+	fail[0]=maxn-1;
+	for (int i=0;i<4;++i)
+		ne[maxn-1][i]=0;
+	memset(vis,0,sizeof(vis));
+	rt=newnode();
+}
+void ins(char* s){
+	int p=rt;
+	for (int i=0;s[i];++i){
+		int c=hs[s[i]];
+		if (!ne[p][c])
+			ne[p][c]=newnode();
+		p=ne[p][c];
+	}
+	vis[p]=1;
+}
+void build(){
+	vector<int> v;
+	v.pb(rt);
+	for (int i=0;i<sz(v);++i){
+		int c=v[i];
+		for (int j=0;j<4;++j){
+			if (ne[c][j]){
+				v.pb(ne[c][j]);
+				fail[ne[c][j]]=ne[fail[c]][j];
+			}
+			else
+				ne[c][j]=ne[fail[c]][j];
+			vis[ne[c][j]]|=vis[ne[fail[c]][j]];// 凡是 fail指针 指向死态的点, 本身也是死态 ！！ 
+		}
+	}
+}
+char s[maxn];
+int main()
+{
+	int n,m;
+	while (scanf("%d%d",&m,&n)!=EOF){
+		init();
+		for (int i=1;i<=m;++i){
+			scanf("%s",s);
+			ins(s);
+		}
+		build();
+		matrix T(id+1,id+1,0);
+		for (int i=0;i<=id;++i){
+			if (vis[i])
+				continue;
+			for (int j=0;j<4;++j)
+				if (!vis[ne[i][j]])
+					T.a[i][ne[i][j]]++;
+		}
+		matrix res=T^n;
+		ll ans=0;
+		for (int i=0;i<=id;++i)
+			ans=(ans+res.a[0][i])%mod;
+		cout<<ans<<"\n";
+	}
+	return 0;
+} 
