b72a10f132c94400cac15097740616744be7b73e
陈金金杰 <739917425@qq.com>
2019-10-02 18:31:47 +0800

    update Convexhull

--- Template/Geo/ConvexHull.cpp
+++ Template/Geo/ConvexHull.cpp
@@ -1,63 +1,66 @@
-#include<iostream>
-#include<cstdio>
-#include<cmath>
-#include<cstring>
-#include<algorithm>
-#define de(x) cout<<#x<<" = "<<x<<endl
-#define dd(x) cout<<#x<<" = "<<x<<" "
-using namespace std;
-const double eps=1e-8,pi=acos(-1);
-const int maxn=1e4+10;
-P p[maxn],ch[maxn];
-int sign(double x)
-{
-	return (x>eps)-(x<-eps);
-}
-struct P
-{
+const double eps=1e-8;
+struct P{
 	double x,y;
 	P(){}
-	P(double a,double b):x(a),y(b){}
-	bool operator<(const P& c) const
-	{
-		int f=sign(x-c.x);
-		return f?f<0:sign(y-c.y)<0;
-	}
-	P operator-(const P& c) 
-	{
-		return P(x-c.x,y-c.y);
+	P(double _x,double _y):x(_x),y(_y){}
+	P operator - (const P& t){
+		return P(x-t.x,y-t.y);
 	}
 };
-double norm(P a)
-{
-	return a.x*a.x+a.y*a.y;
+inline int sign(double x){
+	return (x>eps)-(x<-eps);
 }
-double dis(P a,P b)
-{
-	return sqrt(norm(a-b));
+inline double cross(P a,P b){
+	return a.x*b.y-a.y*b.x;
 }
-double cross(P a,P b)
-{
-	return a.x*b.y-b.x*a.y;
+inline double cross(P a,P b,P c){	// ab 叉乘 ac 
+	return (b.x-a.x)*(c.y-a.y)-(b.y-a.y)*(c.x-a.x);
 }
-int convexhull(P* p,int n,P* ch)
-{
-	sort(p,p+n);
-	int m=0;
-	for (int i=0;i<n;++i)
-	{
-		while (m>1&&sign(cross(ch[m-1]-ch[m-2],p[i]-ch[m-2]))<=0) --m;
-		ch[m++]=p[i];
+inline double dis(P a,P b){
+	return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+}
+// 判断点 q 与凸包 p 的关系, 外:0, 内:1, 边上:-1 
+int inConvex(P q,P* p,int n){
+	int l=1,r=n-2,pos=-1;
+	while (l<=r){
+		int m=l+r>>1;
+		int s1=sign(cross(p[0],q,p[m])),s2=sign(cross(p[0],q,p[m+1]));
+		if (s1<=0&&s2>=0){
+			double tmp=sign(cross(p[m],q,p[m+1]));
+			return tmp?(tmp<0):-1;
+		}
+		if (s2<0)
+			l=m+1;
+		else
+			r=m-1;
 	}
-	int k=m;
-	for (int i=n-2;i>=0;--i)
-	{
-		while (m>k&&sign(cross(ch[m-1]-ch[m-2],p[i]-ch[m-2]))<=0) --m;
-		ch[m++]=p[i];
-	}
-	if (n>1) --m;
-	return m;
+	return 0;
 }
+P p[maxn],hull[maxn];
+bool cmp(const P& b,const P& c){
+	double tmp=cross(b,c,p[0]);
+	if (tmp>eps) return 1;
+	if (fabs(tmp)<eps&&dis(b,p[0])<dis(c,p[0])) return 1;
+	return 0;
+}
+int graham(P* hull,P* p,int n){		// p 下标从 0 开始, 返回凸包点数, 此时p[0]为凸包左下角的点 
+	int k=0;
+	for (int i=1;i<n;++i)
+		if (sign(p[k].y-p[i].y)>0||(!sign(p[i].y-p[k].y)&&sign(p[k].x-p[i].x)>0))
+			k=i;
+	swap(p[0],p[k]);
+	sort(p+1,p+n,cmp);
+	for (int i=0;i<3;++i)
+		hull[i]=p[i];
+	if (n<3) return n;
+	int top=3;
+	for (int i=3;i<n;++i){
+		while (top>=2&&cross(hull[top-2],hull[top-1],p[i])<eps) --top;
+		hull[top++]=p[i];
+	}
+	return top;
+}
+// 求多边形面积 
 double area(P* p,int n)
 {
 	double ans=0;
@@ -66,6 +69,7 @@ double area(P* p,int n)
 		ans+=cross(p[i],p[i+1]);
 	return fabs(ans)/2;
 }
+// 求多边形边长 
 double getlen(P* p,int n)
 {
 	double ans=0;
