9c669cd120db8557015c46c8408d61e32e5e41e8
陈金金杰 <739917425@qq.com>
2019-10-11 18:49:20 +0800

    mupdate 高维前缀和

---
+++ Template/Others/枚举子集&超集.cpp
@@ -0,0 +1,13 @@
+// 枚举s的子集
+for (int i=s;i;i=(i-1)&s)
+	//do...
+// 枚举0~2^m-1的子集
+for (int i=1;i<(1<<m);i<<=1)
+	for (int j=0;j<(1<<m);++j)
+		if (j&i)
+			f[j]+=f[j^i];
+// 枚举0~2^m-1的超集
+for (int i=1;i<(1<<m);i<<=1)
+	for (int j=0;j<(1<<m);++j)
+		if (!(j&i))
+			f[j]+=f[j^i];
