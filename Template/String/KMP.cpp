e83e0289731d62d8914081d0058d9fb360be2778
陈金金杰 <739917425@qq.com>
2020-07-01 11:44:31 +0800

    update KMP

--- Template/String/KMP.cpp
+++ Template/String/KMP.cpp
@@ -1,40 +1,29 @@
 int ne[maxn];
-void init(char *s)
-{
-	ne[0]=-1;
-	int len=strlen(s);
-	for (int i=1;i<len;++i)
-	{
-		int j=ne[i-1];
-		while (s[j+1]!=s[i]&&j>=0)
-			j=ne[j];
-		if (s[j+1]==s[i])
-			ne[i]=j+1;
-		else
-			ne[i]=-1;
-	}
+void init(char s[maxn],int n){	//index from 1 
+	ne[1]=0;
+	for (int i=2,j=0;i<=n;++i){
+		while (s[j+1]!=s[i]&&j)	j=ne[j];
+		j+=s[j+1]==s[i];
+		ne[i]=j;
+	}	
 }
-int match(char* a,char* b)
-{
-	int n=strlen(a),m=strlen(b),cnt=0;
-	while (i<n)
-	{
-		if (a[i]==b[j])
-		{
+void match(char s[maxn],int n,char t[maxn],int m){	//get all positon of s appear in t
+	int i=1,j=1;
+	while (j<=m){
+		if (s[i]==t[j]){
 			++i,++j;
-			if (j==m)
-			{
-				cnt++;
-				j=ne[j-1]+1;
+			if (i>n){
+				/*	
+					matched 
+					start position = j - n
+					do...
+				*/
+				i=ne[i-1]+1;
 			}
 		}
-		else
-		{
-			if (j==0)
-				++i;
-			else
-				j=ne[j-1]+1;
+		else{
+			if (i==1)	++j;
+			else	i=ne[i-1]+1;
 		}
 	}
-	return cnt;
 }
