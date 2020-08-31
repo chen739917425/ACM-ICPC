d03ef9ff224b75a02aa096bd194780e1d255b94a
陈金金杰 <739917425@qq.com>
2020-08-31 16:39:13 +0800

    update

--- Template/Others/Mo.cpp
+++ Template/Others/Mo.cpp
@@ -1,3 +1,4 @@
+//树上莫队
 int bs=450,bl[maxn<<1];		
 int fa[maxn][25],dep[maxn],st[maxn],ed[maxn],ver[maxn<<1],tim;
 int Ans,vis[maxn];
@@ -63,4 +64,96 @@ void Mo(int n){
 		qr[i].ans=Ans;
 		if (qr[i].lca)	Add(qr[i].lca);
 	}
+}
+//回滚莫队
+// 只加不减型
+inline void insert(int p,long long &Maxval)
+{
+    cnt[val[p]]++;
+    Maxval = max( Maxval , 1LL * cnt[val[p]] * a[p] );
+}
+inline void resume(int p)
+{
+    cnt[val[p]]--;
+}
+inline void CaptainMo(void)
+{
+    sort( q+1 , q+m+1 , compare );
+    int l = 1 , r = 0 , lastblock = 0;
+    for (int i=1;i<=m;i++)
+    {
+        // 处理同一块中的询问
+        if ( belo[q[i].l] == belo[q[i].r] )
+        {
+            for (int j=q[i].l;j<=q[i].r;j++) cnt_[val[j]]++;
+            long long temp = 0;
+            for (int j=q[i].l;j<=q[i].r;j++) 
+                temp = max( temp , 1LL * cnt_[val[j]] * a[j] );
+            for (int j=q[i].l;j<=q[i].r;j++) cnt_[val[j]]--;
+            ans[ q[i].id ] = temp; 
+            continue; 
+        }
+        // 如果移动到了一个新的块，就先把左右端点初始化
+        if ( lastblock ^ belo[q[i].l] )
+        {
+            while ( r > R[belo[q[i].l]] ) resume(r--);
+            while ( l < R[belo[q[i].l]]+1 ) resume(l++);
+            Max = 0 , lastblock = belo[q[i].l];
+        }
+        // 单调地移动右端点
+        while ( r < q[i].r ) insert(++r,Max);
+        // 移动左端点回答询问
+        long long temp = Max; int  l_ = l;
+        while ( l_ > q[i].l ) insert(--l_,temp);
+        // 回滚
+        while ( l_ < l ) resume(l_++);
+        ans[ q[i].id ] = temp;
+    }
+}
+//只减不加型
+inline void remove(int p,int &Minval) 
+{
+    if ( a[p] > n+1 ) return;
+    cnt[a[p]]--;
+    if ( cnt[a[p]] == 0 ) Minval = min( Minval , a[p] );
+}
+inline void resume(int p)
+{
+    if ( a[p] > n+1 ) return;
+    cnt[a[p]]++;
+}
+inline void CaptainMo(void)
+{
+    sort( q+1 , q+m+1 , compare );
+    int l = 1 , r = n , lastblock = 0;
+    for (int i=1;i<=m;i++)
+    {
+        // 处理同一块中的询问
+        if ( belo[q[i].l] == belo[q[i].r] )
+        {
+            for (int j=q[i].l;j<=q[i].r;j++) 
+                if ( a[j] <= n+1 ) cnt_[a[j]]++;
+            int temp = 0;
+            while ( cnt_[temp] ) temp++;
+            ans[ q[i].id ] = temp;
+            for (int j=q[i].l;j<=q[i].r;j++) 
+                if ( a[j] <= n+1 ) cnt_[a[j]]--;
+            continue;
+        }
+        // 如果移动到了一个新的块，就先把左右端点初始化
+        if ( belo[q[i].l] ^ lastblock )
+        {
+            while ( r < n ) resume(++r);
+            while ( l < L[belo[q[i].l]] ) remove(l++,ans_);
+            Min = ans_ , lastblock = belo[q[i].l];
+        }
+        // 单调地移动右端点
+        while ( r > q[i].r ) remove(r--,Min);
+        // 移动左端点回答询问
+        int temp = Min , l_ = l;
+        while ( l_ < q[i].l ) remove(l_++,temp);
+        // 回滚
+        while ( l_ > l ) resume(--l_);
+        ans[ q[i].id ] = temp;
+    }
 }
