03e97e91ab10da8c9874602463b5146e57d200f0
陈金金杰 <739917425@qq.com>
2020-10-29 01:21:27 +0800

    update

--- Template/Others/head.cpp
+++ Template/Others/head.cpp
@@ -1,6 +1,7 @@
-#include<bits/stdc++.h> 
+#include<bits/stdc++.h>
 #define dd(x) cout<<#x<<" = "<<x<<" "
 #define de(x) cout<<#x<<" = "<<x<<endl
+#define de_arr(a,s,t) cout<<#a<<": ";for (int z=(s);z<=(t);++z)cout<<a[z]<<" ";cout<<endl;
 #define sz(x) int(x.size())
 #define All(x) x.begin(),x.end()
 #define pb push_back
@@ -11,11 +12,12 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 typedef pair<int,int> P;
-typedef priority_queue<int> BQ;
-typedef priority_queue<int,vector<int>,greater<int> > SQ;
-const int maxn=1e5+10,mod=1e9+7,INF=0x3f3f3f3f;
+const double eps=1e-8;
+inline int sign(double x){return (x>eps)-(x<-eps);}
+const int maxn=5e5+10,mod=1e9+7,INF=0x3f3f3f3f;
+
 int main()
 {
-	
+
 	return 0;
 }
